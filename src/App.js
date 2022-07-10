751b30196cf5c9889f0788365cec9d56cb75b3ab
Hongsinwon <ghdtlsdnjs12@naver.com>
2022-07-11 00:21:33 +0900

    스터디 클론코딩 styled-components 작업

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Header } from "./components/organisms";
+import { Main, Search, VideoDetail } from "./components/pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/search/:id" element={<Search />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
